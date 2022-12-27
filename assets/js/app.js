540571576a8752ff7dae06c65c2c845e7b077e6d
Felipe Martini <felipecmartini@gmail.com>
2022-12-27 10:05:58 -0300

    about grid added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,26 @@
 const menuItems = document.querySelectorAll('.navbar-menu a');
 
-console.log(menuItems);
-
 menuItems.forEach(item => {
     item.addEventListener('click', scrollToIdOnClick)
 })
-
-function scrollToIdOnClick(event) {
-    event.preventDefault();
-
-    const to = getScrollTopByHref(event.target);
-
-    window.scroll({
-        top: to - 160,
-        behavior: "smooth"
-    })
-}
 
 function getScrollTopByHref (element) {
     const id = element.getAttribute('href');
     return document.querySelector(id).offsetTop;
 }
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+
+    const to = getScrollTopByHref(event.target) - 96;
+
+    scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: "smooth"
+    })
+}
+
