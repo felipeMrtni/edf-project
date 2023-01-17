e34eab318e4ac1bec79251a0433d250ad2274fc3
Felipe Martini <felipecmartini@gmail.com>
2023-01-17 18:13:19 -0300

    mobile menu added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,29 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar-logo');
+
+//display mobile Menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu);
+
+// close mobile menu when clicking on a menu item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+}
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+
+// --------------------------------------------------------------------
+// HIGHLIGHT MENU
 const menuItems = document.querySelectorAll('.navbar-container a');
 
 menuItems.forEach(item => {
@@ -102,7 +128,7 @@ const highlightMenu = () => {
         servicesMenu.classList.remove('highlight');
         contactMenu.classList.remove('highlight');
         return;
-    } else if (window.innerWidth > 800 && scrollPos < 5000) {
+    } else if (window.innerWidth > 800 && scrollPos < 6000) {
         contactMenu.classList.add('highlight');
         clientMenu.classList.remove('highlight');
         return;
